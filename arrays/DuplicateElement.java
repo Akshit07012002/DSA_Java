b713044771dc0a66a363e2ea0596157afde1da6a
Akshit Mahaur <mahaurakshit7@gmail.com>
2022-07-24 21:11:33 +0530

    DuplicateElement done

--- arrays/DuplicateElement.java
+++ arrays/DuplicateElement.java
@@ -1,6 +1,8 @@
 package arrays;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DuplicateElement {
 
@@ -9,18 +11,33 @@ public class DuplicateElement {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(in.readLine());
-
+        int i = 0;
         int[] a = new int[size];
 
         String[] s = new String[1];
 
         s = in.readLine().split(" ");
 
-        for(int i=0; i<s.length; i++)
+        for(i=0; i<s.length; i++)
             a[i] = Integer.parseInt(s[i]);
 
-        for(int i=0; i<s.length; i++)
-            System.out.println(a[i]);
+        Set<Integer> uniqueNum = new HashSet<>(); // unordered collection of objects which disregards duplicate values
+        boolean duplicateFound = false; // flag
+
+        // push each element of array into the set whilst checking for duplicates
+        for(i=0; i<s.length; i++){
+
+            if(uniqueNum.contains(a[i])){
+                duplicateFound = true;
+                System.out.println("Duplicate Element Found : " + a[i]);
+            }
+            else
+                uniqueNum.add(a[i]);
+        }
+
+        // no duplicates found
+        if(!duplicateFound)
+            System.out.println("-1");
 
     }
 
